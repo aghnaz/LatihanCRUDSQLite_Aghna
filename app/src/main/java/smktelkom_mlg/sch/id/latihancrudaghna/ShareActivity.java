a3485a8f02acaca6b18101c83d65287279429e15
aghnaz <aghna_zhalila_26rpl@student.smktelkom-mlg.sch.id>
2019-04-13 04:51:55 +0700

    First commit

---
+++ app/src/main/java/smktelkom_mlg/sch/id/latihancrudaghna/ShareActivity.java
@@ -0,0 +1,13 @@
+package smktelkom_mlg.sch.id.latihancrudaghna;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ShareActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_share);
+    }
+}
